d7ad883388f4f9a5efbf9bae1c0c9d05beb3183c
beepscore <support@beepscore.com>
2012-12-24 15:43:51 -0800

    Update path to python3.

--- 02 Quick Start/exceptions.py
+++ 02 Quick Start/exceptions.py
@@ -1,4 +1,5 @@
-#!/usr/bin/python3
+#!/usr/local/bin/python3
+
 # exceptions.py by Bill Weinman [http://bw.org/]
 # This is an exercise file from Python 3 Essential Training on lynda.com
 # Copyright 2010 The BearHeart Gorup, LLC
