057e849c822d5b40d7a8e086fcbe31e969363199
beepscore <support@beepscore.com>
2012-12-23 23:49:18 -0800

    Add comment about Macvim Janus Syntastic warns syntax error even though script runs ok.

--- 02 Quick Start/forloop.py
+++ 02 Quick Start/forloop.py
@@ -3,5 +3,7 @@
 # read the lines from the file
 fh = open('lines.txt')
 for line in fh.readlines():
-    # set end to empty string to override default '\n'
+    # In Python 3, print() is a function and requires parentheses.
+    # Set print end to empty string '' instead of default newline '\n'
+    # After setting end, Janus Macvim Syntastic plugin warns syntax error but script runs ok.
     print(line, end='')
