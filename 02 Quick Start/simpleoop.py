69755439eb65799d892da95893d7ba76e6d34415
beepscore <support@beepscore.com>
2012-12-24 15:07:20 -0800

    Add comments.

--- 02 Quick Start/simpleoop.py
+++ 02 Quick Start/simpleoop.py
@@ -3,15 +3,18 @@
 # simple fibonacci series
 # the sum of two elements defines the next set
 class Fibonacci():
+    # __init__() is a constructor
     def __init__(self, a, b):
         self.a = a
         self.b = b
 
+    # series() is a generator
     def series(self):
         while(True):
             yield(self.b)
             self.a, self.b = self.b, self.a + self.b
 
+# instantiate an instance of class Fibonacci.
 f = Fibonacci(0, 1)
 for r in f.series():
     if r > 100: break    
