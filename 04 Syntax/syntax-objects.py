8a53b4decf356e0800dbb3fae5158631aa3fbe52
beepscore <support@beepscore.com>
2012-12-25 08:55:22 -0800

    Add syntax-objects.py

---
+++ 04 Syntax/syntax-objects.py
@@ -0,0 +1,21 @@
+#!/usr/local/bin/python3
+
+# syntax-objects.py by Bill Weinman [http://bw.org/]
+# This is an exercise file from Python 3 Essential Training on lynda.com
+# Copyright 2010 The BearHeart Group, LLC
+
+class Egg:
+    # In a class method, always list self as first parameter
+    def __init__(self, kind = "fried"):
+        self.kind = kind
+
+    def whatKind(self):
+       return self.kind 
+
+def main():
+    fried = Egg()
+    scrambled = Egg('scrambled')
+    print("fried.whatKind():{0}".format(fried.whatKind()))
+    print("scrambled.whatKind():{0}".format(scrambled.whatKind()))
+
+if __name__ == "__main__": main()
