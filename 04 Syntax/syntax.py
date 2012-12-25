da1ea35407cf577ae5cda64e2a16186735671a3b
beepscore <support@beepscore.com>
2012-12-24 17:46:53 -0800

    Improve comments about use of main()

--- 04 Syntax/syntax.py
+++ 04 Syntax/syntax.py
@@ -4,8 +4,12 @@
 # This is an exercise file from Python 3 Essential Training on lynda.com
 # Copyright 2010 The BearHeart Group, LLC
 
-# Using main() allows calling any function as long as it appears somewhere in the script.
-# If not using main(), function must appear before calling it.
+# An interpreted language such as Python has no compiler checking.
+# A statement won't be evaluated unless/until execution reaches it.
+# By declaring function main() and waiting until the end of the file to call main(),
+# the interpreter will have scanned all declarations in the file before it attempts to use them.
+# This allows declaring functions in any order.
+# Without this, a declaration would have to appear before it is used.
 def main():
     print("This is the syntax.py file.")
     egg()
@@ -13,6 +17,7 @@ def main():
 def egg():
     print("egg")
 
-# If script is run as the main module (e.g. from the command line), __name__ will equal "__main__" and the script will call main()
+# Python scripts often end with this line.
+# If script is run as the main module (e.g. from the command line), __name__ will equal "__main__" and when the interpreter reaches the bottom of the file it will call main()
 # If script is included in a different module, it won't automatically call main().
 if __name__ == "__main__": main()
