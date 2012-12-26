65334d0ac4f74e0298df07342cfd43e5df1beaf8
beepscore <support@beepscore.com>
2012-12-25 21:54:44 -0800

    Show number operations.

--- 05 Variables/variable-numbers.py
+++ 05 Variables/variable-numbers.py
@@ -4,7 +4,20 @@
 # Copyright 2010 The BearHeart Group, LLC
 
 def main():
-    num = 42
-    print(num)
+
+    # in Python dividing two integers doesn't truncate
+    # integer division is //
+    intResult = 42//9
+
+    floatResult = 42/9
+    roundTwoResult = round(42/9, 2)
+    roundResult = round(42/9)
+    print(intResult, floatResult, roundTwoResult, roundResult)
+
+    moduloResult = 42 % 9
+    print(moduloResult)
+
+    # float is a constructor, instantiates a float, param is int
+    a = float(42)
 
 if __name__ == "__main__": main()
