78ba4a4ce98802f9cc472368a233c8a1e99c3715
beepscore <support@beepscore.com>
2012-12-31 14:34:05 -0800

    Add conditionals.py

--- 06 Conditionals/conditionals.py
+++ 06 Conditionals/conditionals.py
@@ -1,9 +1,13 @@
-#!/usr/bin/python3
+#!/usr/local/bin/python3
 # conditionals.py by Bill Weinman [http://bw.org/]
 # This is an exercise file from Python 3 Essential Training on lynda.com
 # Copyright 2010 The BearHeart Group, LLC
 
 def main():
-    print("This is the conditionals.py file.")
+    a, b = 0, 1
+    if a < b:
+        print('this is true')
+    else:
+        print('this is false')
 
 if __name__ == "__main__": main()
