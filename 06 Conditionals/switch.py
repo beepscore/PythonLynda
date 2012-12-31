7ec882acfb4c2df265b8518f60dee413ecf4cbe4
beepscore <support@beepscore.com>
2012-12-31 14:59:37 -0800

    Modify switch.py

--- 06 Conditionals/switch.py
+++ 06 Conditionals/switch.py
@@ -1,9 +1,27 @@
-#!/usr/bin/python3
+#!/usr/local/bin/python3
 # switch.py by Bill Weinman [http://bw.org/]
 # This is an exercise file from Python 3 Essential Training on lynda.com
 # Copyright 2010 The BearHeart Group, LLC
 
 def main():
-    print('this is the switch.py file')
+
+    # Python doesn't have a switch statement
+    # You can do something similar using a dictionary
+    choices = dict(
+            one = 'first',
+            two = 'second',
+            three = 'third',
+            four = 'fourth',
+            five = 'fifth',
+    )
+    v = 'three'
+
+    # If dictionary doesn't have an entry for the key, this will give KeyError
+    print('choices[v]: {}'.format(choices[v]))
+
+    # handle default
+    v = 'bazillion'
+    # if get doesn't find entry for the key, it will return specified default
+    print('choices.get(v, "other"): {}'.format(choices.get(v, "other")))
 
 if __name__ == "__main__": main()
