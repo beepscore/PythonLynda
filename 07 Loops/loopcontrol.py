2bfec8e3ce7b4422ac0b9501ab5792152c2ca97a
beepscore <support@beepscore.com>
2013-01-01 13:40:48 -0800

    In loopcontrol.py add continue, break, and else.

--- 07 Loops/loopcontrol.py
+++ 07 Loops/loopcontrol.py
@@ -1,11 +1,27 @@
-#!/usr/bin/python3
-# break.py by Bill Weinman [http://bw.org/]
+#!/usr/local/bin/python3
+# loopcontrol.py by Bill Weinman [http://bw.org/]
 # This is an exercise file from Python 3 Essential Training on lynda.com
-# Copyright 2010 The BearHeart Group, LLC
+# Portions copyright 2010 The BearHeart Group, LLC
 
 def main():
     s = 'this is a string'
+
+    for c in s:
+        # continue exits this iteration
+        if 's' == c: continue
+        print(c, end='')
+    print()
+
+    for c in s:
+        # break exits the entire loop
+        if 's' == c: break
+        print(c, end='')
+    print()
+
     for c in s:
         print(c, end='')
+    else:
+        print()
+        print('Loop finished')
 
 if __name__ == "__main__": main()
