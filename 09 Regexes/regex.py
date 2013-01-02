609c18fefa12d7bec76e1dfc1e9219dc494f9a1d
beepscore <support@beepscore.com>
2013-01-01 18:49:56 -0800

    In regex.py print group, add regular expression to match Nevermore or nevermore. I imagine this could be terser.

--- 09 Regexes/regex.py
+++ 09 Regexes/regex.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/local/bin/python3
 # regex.py by Bill Weinman [http://bw.org/]
 # This is an exercise file from Python 3 Essential Training on lynda.com
 # Copyright 2010 The BearHeart Gorup, LLC
@@ -10,5 +10,22 @@ def main():
     for line in fh:
         if re.search('(Len|Neverm)ore', line):
             print(line, end='')
+    print()
+
+    # after running loop above, must re-assign fh. Why?
+    # Does python automatically close file when iterator reaches the end?
+    fh = open('raven.txt')
+    for line in fh:
+        match = re.search('(Len|Neverm)ore', line)
+        if match:
+            print(match.group())
+    print()
+
+    fh = open('raven.txt')
+    for line in fh:
+        match = re.search('(Len|Neverm|neverm)ore', line)
+        if match:
+            print(match.group())
+    print()
 
 if __name__ == "__main__": main()
