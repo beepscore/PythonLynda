f92978872f90d4c73ecaf6f478478dc2e1b5db6e
beepscore <support@beepscore.com>
2013-01-01 19:18:47 -0800

    Add search, then replace.

--- 09 Regexes/regex.py
+++ 09 Regexes/regex.py
@@ -11,6 +11,7 @@ def main():
     for line in fh:
         if re.search('(Len|Neverm)ore', line):
             print(line, end='')
+    print('-------------------------------')
     print()
 
     # after running loop above, must re-assign fh. Why?
@@ -20,20 +21,36 @@ def main():
         match = re.search('(Len|Neverm)ore', line)
         if match:
             print(match.group())
+    print('-------------------------------')
     print()
 
+    print('widen match')
     fh = open('raven.txt')
     for line in fh:
         match = re.search('(Len|Neverm|neverm)ore', line)
         if match:
             print(match.group())
+    print('-------------------------------')
     print()
 
+    print('substitute, i.e. search and replace')
     fh = open('raven.txt')
     for line in fh:
         # print every line
-        # substitute, i.e. search and replace
         print(re.sub('(Len|Neverm|neverm)ore', 'Al Gore', line), end='')
+    print('-------------------------------')
+    print()
+
+    print('first search, then replace')
+    fh = open('raven.txt')
+    for line in fh:
+        # search
+        match = re.search('(Len|Neverm|neverm)ore', line)
+        if match:
+            # replace
+            #print(re.sub('(Len|Neverm|neverm)ore', 'Al Gore', match.group()))
+            print(line.replace(match.group(), 'Al Gore'), end='')
+    print('-------------------------------')
     print()
 
 if __name__ == "__main__": main()
