5e4aacbe9a69f42654a9468a0e137b313b477efc
beepscore <support@beepscore.com>
2013-01-03 22:47:57 -0800

    In functions.py add functions with default argument values.

--- 11 Functions/functions.py
+++ 11 Functions/functions.py
@@ -1,12 +1,25 @@
-#!/usr/bin/python3
+#!/usr/local/bin/python3
 # functions.py by Bill Weinman [http://bw.org/]
 # This is an exercise file from Python 3 Essential Training on lynda.com
 # Copyright 2010 The BearHeart Group, LLC
 
 def main():
-    testfunc()
+    emptyFunc()
+    testFuncOne(42, 16)
+    testFuncTwo(38)
 
-def testfunc():
-    print('This is a test function')
+def emptyFunc():
+    # pass acts like a nop, can be used to stub a function or method body and avoid syntax error.
+    pass
+
+# assigning default parameter value in function definition means caller can omit it.
+def testFuncOne(number, another = 43, oneMore = 75):
+    print('testFuncOne:', number, another, oneMore)
+
+# None is a system singleton object
+def testFuncTwo(number, another = None, oneMore = 75):
+    print('testFuncTwo:', number, another, oneMore)
+    if another is None : another = 112
+    print('testFuncTwo:', number, another, oneMore)
 
 if __name__ == "__main__": main()
