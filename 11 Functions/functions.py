3d394f640656bb3ec0cf9d18ada7658eca0f227b
beepscore <support@beepscore.com>
2013-01-05 11:56:26 -0800

    Add funcWithKeywordArgs

--- 11 Functions/functions.py
+++ 11 Functions/functions.py
@@ -9,6 +9,10 @@ def main():
     testFuncTwo(38)
     testFuncThree(1, 2, 3, 42, 43, 45, 46)
 
+    # caller names the arguments, not the function def
+    funcWithKeywordArgs(one = 1, two = 2, four = 42)
+
+
 def emptyFunc():
     # pass acts like a nop, can be used to stub a function or method body and avoid syntax error.
     pass
@@ -33,6 +37,19 @@ def testFuncThree(this, that, other, *args):
     # this prints args as a tuple
     # output: testFuncThree: 1 2 3 (42, 43, 45, 46)
     print('testFuncThree:', this, that, other, args)
+
+    # tuple will print in order
     for n in args: print(n, end = ' ')
+    print()
+
+
+# kwargs is a dictionary, caller specifies keys
+# ==> Function doesn't know names before it is called.
+def funcWithKeywordArgs(**kwargs):
+    print('funcWithKeywordArgs:', kwargs['one'], kwargs['two'], kwargs['four'])
+
+    # kwargs dictionary items may appear in any order
+    for key in kwargs:
+        print(key, kwargs[key])
 
 if __name__ == "__main__": main()
