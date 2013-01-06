f5d0c6d8f94e4d05870c2a3adff8ee456c3bcc87
beepscore <support@beepscore.com>
2013-01-06 12:00:37 -0800

    Add instance variable '_value'.

--- 12 Classes/classes.py
+++ 12 Classes/classes.py
@@ -5,24 +5,36 @@
 
 class Duck:
 
-    def __init__(self):
+    def __init__(self, value):
+        self._value = value
         print('constructor')
 
 
     def quack(self):
-        print('Quaaack!')
+        print('Quaaack!', self._value)
 
     def walk(self):
-        print('Walks like a duck.')
+        print('Walks like a duck.', self._value)
 
 def main():
 
     #instantiate a Duck, calls the constructor __init__
-    donald = Duck()
+    donald = Duck(47)
     print(donald)
+    frank = Duck(151)
+    print(frank)
 
     # method call implicitly supplies argument 'self', a reference to the caller
     donald.quack()
     donald.walk()
+    frank.quack()
+    frank.walk()
+
+    print()
+
+    # can access an object's instance variable directly, but best practice is to use a getter and setter.
+    print(donald._value)
+    donald._value = 'boo'
+    print(donald._value)
 
 if __name__ == "__main__": main()
