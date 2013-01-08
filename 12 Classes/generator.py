34d61851401a2dbc6ab111d1f480b3d56a2b4594
beepscore <support@beepscore.com>
2013-01-07 21:39:12 -0800

    In Chapter 11 defined inclusive_range as a function. Here in Chapter 12 define inclusive_range as a class.

--- 12 Classes/generator.py
+++ 12 Classes/generator.py
@@ -1,10 +1,56 @@
-#!/usr/bin/python3
-# classes.py by Bill Weinman [http://bw.org/]
+#!/usr/local/bin/python3
+# generator.py by Bill Weinman [http://bw.org/]
 # This is an exercise file from Python 3 Essential Training on lynda.com
 # Copyright 2010 The BearHeart Group, LLC
 
+# In Chapter 11 defined inclusive_range as a function.
+# Here in Chapter 12 define inclusive_range as a class.
+
+# Python standard range is non-inclusive of stop.
+# Define an inclusive range
+class inclusive_range:
+
+    def __init__(self, *args):
+        # Allow call with one argument stop, 
+        # two arguments start, stop, 
+        # or three arguments start, stop, step.
+        numberOfArguments = len(args)
+
+        if numberOfArguments < 1:
+            raise TypeError('inclusive_range caller must supply at least one argument stop')
+        elif numberOfArguments ==  1:
+            self._start = 0
+            self._stop = args[0]
+            self._step = 1
+        elif numberOfArguments ==  2:
+            (self._start, self._stop) = args
+            self._step = 1
+        elif numberOfArguments ==  3:
+            (self._start, self._stop, self._step) = args
+        else:
+            message = 'inclusive_range expected at most three arguments, got {}'.format(numberOfArguments)
+            raise TypeError(message)
+
+
+    # implementing __iter__ makes object iterable when used in an iterative context such as a loop.
+    # returns an generator object.
+    def __iter__(self):
+        i = self._start
+        while i <= self._stop:
+            # yield returns a value, then resumes loop.
+            yield i
+            i += self._step
+
+
 def main():
-    o = range(25)
+
+    # instantiate an inclusive range
+    # o = inclusive_range(25)
+    # o = inclusive_range(5, 25, 7)
+    # o = inclusive_range()
+    # o = inclusive_range(5, 25, 7, 38)
+    o = inclusive_range(5, 25, 2)
+
     for i in o: print(i, end = ' ')
 
 if __name__ == "__main__": main()
