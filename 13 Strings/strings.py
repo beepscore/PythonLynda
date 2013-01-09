c195091b62f7070a7c0ad09e5f12468e2da43159
beepscore <support@beepscore.com>
2013-01-08 21:54:14 -0800

    Add rstrip() example, add comments.

--- 13 Strings/strings.py
+++ 13 Strings/strings.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/local/bin/python3
 # strings.py by Bill Weinman [http://bw.org/]
 # This is an exercise file from Python 3 Essential Training on lynda.com
 # Copyright 2010 The BearHeart Group, LLC
@@ -9,9 +9,18 @@ def main():
     print(s.title())
     print(s.upper())
     print(s.swapcase())
+    print()
+    
+    # find returns the index of the first letter. Finds 'is' in this, returns 2.
     print(s.find('is'))
+
     print(s.replace('this', 'that'))
+    # strips from beginning and end of string. Default is to strip whitespace, including newline.
     print(s.strip())
+    # rstrip() removes from right end only.
+    print('blah\n')
+    print('blah\n'.rstrip('\n'))
+
     print(s.isalnum())
     print(s.isalpha())
     print(s.isdigit())
