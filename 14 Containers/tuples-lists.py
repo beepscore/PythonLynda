95b700a5344bef2fab9d04143c582bb0682c3f8b
beepscore <support@beepscore.com>
2013-01-21 22:58:56 -0800

    In tuples-lists.py add list examples.

--- 14 Containers/tuples-lists.py
+++ 14 Containers/tuples-lists.py
@@ -11,10 +11,17 @@ def main():
     print('This is the tuples-lists.py file.')
 
     # tuples, immutable
+    # can create a tuple without ()
     firstTuple = 18, 2, 4
     print('firstTuple', firstTuple) 
     secondTuple = (6, 8, 3, 5)
     print('secondTuple', secondTuple) 
+
+    print('Surrounding a number with () doesn\'t make it a tuple.')
+    print('type((12)) is', type((12)) )
+    print('To declare a tuple, use () and at least one comma.')
+    print('type(12,) is', type((12,)) )
+
 
     print('secondTuple[0]', secondTuple[0]) 
     print('secondTuple[-1]', secondTuple[-1]) 
@@ -22,17 +29,18 @@ def main():
     print('min(secondTuple)', min(secondTuple)) 
     print('max(secondTuple)', max(secondTuple)) 
 
-    print('Surrounding a number with () doesn\'t make it a tuple.')
-    print('type((12)) is', type((12)) )
-    print('To declare a tuple, use () and at least one comma.')
-    print('type(12,) is', type((12,)) )
-
     # immutable, gives TypeError: 'tuple' object does not support item assignment
-    secondTuple[2] = 13
+    # secondTuple[2] = 13
 
 
     # lists, mutable
     print('type[12] is', type([12]) )
+
+    firstList = [6, 8, 3, 5]
+    print('firstList', firstList) 
+
+    firstList[2] = 42
+    print('firstList', firstList) 
 
 
 if __name__ == "__main__": main()
