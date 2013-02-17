4b2ee14e3d96f9af3f4f1f7e4b644e0a1f5b875c
beepscore <support@beepscore.com>
2013-02-17 13:38:07 -0800

    In files.py add comments.

--- 15 Files/files.py
+++ 15 Files/files.py
@@ -1,11 +1,16 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 # files.py by Bill Weinman [http://bw.org/]
 # This is an exercise file from Python 3 Essential Training on lynda.com
 # Copyright 2010 The BearHeart Group, LLC
 
 def main():
-    f = open('lines.txt')
+    # Open file
+    # http://docs.python.org/3/library/functions.html#open
+    # Explicitly specify read mode. Alternatively, if omit, default # is 'r'
+    f = open('lines.txt', 'r')
+
     for line in f:
         print(line, end = '')
+
 
 if __name__ == "__main__": main()
