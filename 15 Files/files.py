fd0626a1b148fe2ba6130492951e7a5091cc024b
beepscore <support@beepscore.com>
2013-02-17 14:35:10 -0800

    In files.py read a big file in buffered I/O chunks, not line by line.

--- 15 Files/files.py
+++ 15 Files/files.py
@@ -10,12 +10,28 @@ def main():
     infile = open('lines.txt', 'r')
     outfile = open('new.txt', 'w')
 
+    # read file line by line
     for line in infile:
         # print to screen
         print(line, end = '')
         # print to outfile
         print(line, file = outfile, end = '')
 
-    print('Done.')
+    print('Done writing new.txt.')
+
+
+    # read big file in buffered I/O chunks, not line by line
+    buffersize = 50000
+    infile = open('bigfile.txt', 'r')
+    outfile = open('bigfileout.txt', 'w')
+
+    buffer = infile.read(buffersize)
+    while len(buffer):
+        print('.', end = '')
+        outfile.write(buffer)
+        buffer = infile.read(buffersize)
+
+    print()
+    print('Done writing bigfileout.txt.')
 
 if __name__ == "__main__": main()
