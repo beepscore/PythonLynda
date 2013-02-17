d456f20081ab34c057b81de76f29893a945bc35f
beepscore <support@beepscore.com>
2013-02-17 15:00:56 -0800

    In files.py read and write a binary file. Revise comments.

--- 15 Files/files.py
+++ 15 Files/files.py
@@ -4,13 +4,17 @@
 # Copyright 2010 The BearHeart Group, LLC
 
 def main():
-    # Open file
+
+    # Python distinguishes between text and binary files,
+    # whether the current operating system does or not.
+
+    # Read and write a text file
     # http://docs.python.org/3/library/functions.html#open
     # Explicitly specify read mode. Alternatively, if omit, default # is 'r'
     infile = open('lines.txt', 'r')
     outfile = open('new.txt', 'w')
 
-    # read file line by line
+    # read file line using line oriented I/O
     for line in infile:
         # print to screen
         print(line, end = '')
@@ -20,7 +24,7 @@ def main():
     print('Done writing new.txt.')
 
 
-    # read big file in buffered I/O chunks, not line by line
+    # As an example, read and write a large text file using buffered I/O.
     buffersize = 50000
     infile = open('bigfile.txt', 'r')
     outfile = open('bigfileout.txt', 'w')
@@ -33,5 +37,23 @@ def main():
 
     print()
     print('Done writing bigfileout.txt.')
+
+
+    # Read and write a binary file
+    # Use buffered I/O.
+    buffersize = 50000
+    # In Python, binary file must be opened in binary mode.
+    infile = open('olives.jpg', 'rb')
+    outfile = open('olivesout.jpg', 'wb')
+
+    buffer = infile.read(buffersize)
+    while len(buffer):
+        print('.', end = '')
+        outfile.write(buffer)
+        buffer = infile.read(buffersize)
+
+    print()
+    print('Done writing olivesout.jpg.')
+
 
 if __name__ == "__main__": main()
