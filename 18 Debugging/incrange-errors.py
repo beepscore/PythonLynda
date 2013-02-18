318911e3c4ae22f6d57d80d38381bf4e84fc8f11
beepscore <support@beepscore.com>
2013-02-18 13:06:21 -0800

    In incrange-errors.py fix errors.

--- 18 Debugging/incrange-errors.py
+++ 18 Debugging/incrange-errors.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 # incrange-errors.py by Bill Weinman [http://bw.org/]
 # This is an exercise file from Python 3 Essential Training on lynda.com
 # Copyright 2010 The BearHeart Group, LLC
@@ -15,17 +15,19 @@ class inclusive_range:
             (self.start, self.stop) = args
             step = 1
         elif numargs == 3:
-            (self.step, self.stop, self.start) = args
+            (self.start, self.stop, self.step) = args
         else: raise TypeError('inclusiveRange expected at most 3 arguments, got {}'.format(numargs))
 
     def __iter__(self):
         i = self.start
-        while i >= self.stop:
+        while i <= self.stop:
             yield i
             i += self.step
 
 def main():
     o = inclusive_range(4, 25, 3)
+    print(o.start, o.stop, o.step)
+    # use o as an iterator
     for i in o: print(i, end = ' ')
 
 if __name__ == "__main__": main()
