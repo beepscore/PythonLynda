458255b870e9c8ec99e64f41a9378b1c5e7bfa23
beepscore <support@beepscore.com>
2013-02-17 16:11:16 -0800

    In databases.py use row factory.

--- 16 Databases/databases.py
+++ 16 Databases/databases.py
@@ -7,6 +7,7 @@
 # Python comes with sqlite
 # sqlite is simple to use. Doesn't require setting up a server
 # mySQL requires setting up a server.
+# http://docs.python.org/3.3/library/sqlite3.html
 import sqlite3
 
 def main():
@@ -23,7 +24,15 @@ def main():
     # Reference:
     # http://bind10.isc.org/wiki/SystemNotesMacOSXMountainLion
 
+    # Can view sqlite database binary file with Firefox plugin SQLiteManager
+
     db = sqlite3.connect('test.sqlite')
+
+    # Row factory is very powerful.
+    # Enables Python to get objects from sqlite
+    # Row provides both index-based and case-insensitive name-based access to columns
+    db.row_factory = sqlite3.Row
+
     # delete old table
     db.execute('drop table if exists test')
     db.execute('create table test (t1 text, i1 int)')
@@ -31,11 +40,14 @@ def main():
     db.execute('insert into test (t1, i1) values (?, ?)', ('two', 2))
     db.execute('insert into test (t1, i1) values (?, ?)', ('three', 3))
     db.execute('insert into test (t1, i1) values (?, ?)', ('four', 4))
+    # after changing data, must commit it.
     db.commit()
+    # sort by t1 text field
     cursor = db.execute('select * from test order by t1')
 
     for row in cursor:
-        print(row)
+        # dictionary objects contain indices
+        print(dict(row))
 
 
 if __name__ == "__main__": main()
