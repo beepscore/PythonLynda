c7d2ca7ba2daea5dadcd141ed0156fc0930066a8
beepscore <support@beepscore.com>
2013-02-18 22:56:09 -0800

    In modules.py import bitstring and use it.
    Downloaded bitstring package and installed:
    $  cd /Users/stevebaker/Documents/pythonProjects/bitstring-3.0.2
    $  python3 setup.py install

--- 17 Modules/modules.py
+++ 17 Modules/modules.py
@@ -7,6 +7,12 @@
 # <http://docs.python.org/3/library/sys.html>
 
 import sys
+import datetime
+import urllib.request
+import random
+# use a module from PyPi
+# https://pypi.python.org/pypi/bitstring/3.0.2
+import bitstring
 
 def main():
     print('Python version {}.{}.{}'.format(*sys.version_info))
@@ -22,23 +28,24 @@ def main():
     # urandom(n) returns n random bytes
     print("os.urandom(25) {}".format(os.urandom(25)))
 
-    import urllib.request
     page = urllib.request.urlopen('http://bw.org/')
     for line in page:
         # print(str(line, encoding = 'utf_8'), end = '')
         pass
 
-    import random
     print(random.randint(1, 1000))
     x = list(range(25))
     print('x {}'.format(x))
     random.shuffle(x)
     print('shuffle(x) {}'.format(x))
 
-    import datetime
     now = datetime.datetime.now()
     print(now)
     print(now.year, now.month, now.day, now.minute, now.second, now.microsecond)
 
+
+    a = bitstring.BitString(bin = '01010101')
+    # expected output 55 01010101 85
+    print(a.hex, a.bin, a.uint)
 
 if __name__ == "__main__": main()
